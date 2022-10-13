9c451af78ab9fbb491cd480b6486ca565108dcc9
seraleman <seraleman1204@gmail.com>
2022-10-12 19:40:53 -0500

    Express basado en clases

--- app.js
+++ app.js
@@ -1,13 +1,5 @@
 require('dotenv').config()
+const Server = require('./models/server')
 
-const express = require('express')
-const app = express()
-const port = process.env.PORT
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.listen(port, () => {
-  console.log(`REST Server App está escuchando en http://localhost:${port}`)
-})
+const server = new Server()
+server.listen()
