96f7fb240f0350783816b09d5c3130566bf397fc
seraleman <seraleman1204@gmail.com>
2022-10-12 20:54:19 -0500

    Separando las rutas y el controlador de la clase

--- models/server.js
+++ models/server.js
@@ -5,6 +5,7 @@ class Server {
   constructor() {
     this.app = express()
     this.port = process.env.PORT
+    this.usersPath = '/api/users'
 
     // Middlewares
     this.middlewares()
@@ -22,29 +23,7 @@ class Server {
   }
 
   routes() {
-    this.app.get('/api', (req, res) => {
-      res.json({
-        msg: 'get API',
-      })
-    })
-
-    this.app.put('/api', (req, res) => {
-      res.status(400).json({
-        msg: 'put API',
-      })
-    })
-
-    this.app.post('/api', (req, res) => {
-      res.status(201).json({
-        msg: 'post API',
-      })
-    })
-
-    this.app.delete('/api', (req, res) => {
-      res.json({
-        msg: 'delete API',
-      })
-    })
+    this.app.use(this.usersPath, require('../routes/user.routes'))
   }
 
   listen() {
