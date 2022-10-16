1673378517c0623064a1b63d680ac2522816bfb7
seraleman <seraleman1204@gmail.com>
2022-10-16 17:53:46 -0500

    Generando un JWT

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,6 +1,7 @@
 const { request, response } = require('express')
 const bcryptjs = require('bcryptjs')
 const User = require('../models/user')
+const generateJWT = require('../helpers/generate-jwt')
 
 const login = async (req = request, res = response) => {
   const { email, password } = req.body
@@ -29,11 +30,11 @@ const login = async (req = request, res = response) => {
     }
 
     // Generar el JWT
+    const token = await generateJWT(user.id)
 
     res.json({
-      msg: 'Login Ok',
-      email,
-      password,
+      user,
+      token,
     })
   } catch (error) {
     console.log(error)
