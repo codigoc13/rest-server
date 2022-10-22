8c7206275a2c1a03b183985ab8f3c59f90981266
seraleman <seraleman1204@gmail.com>
2022-10-22 17:36:39 -0500

    Creando un usuario personalizado con las credenciales de Google

--- models/user.js
+++ models/user.js
@@ -20,6 +20,7 @@ const UserSchema = Schema({
   role: {
     type: String,
     required: [true, 'El rol es obligatorio'],
+    default: 'USER_ROLE',
     // enum: ['ADMIN_ROLE', 'USER_ROLE'],
   },
   status: {
