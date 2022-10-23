66fb6a725a08b4e7c8c915d81cc341829b93dd38
seraleman <seraleman1204@gmail.com>
2022-10-23 13:24:35 -0500

    Tarea de validaciones y CRUD productos

---
+++ routes/product.routes.js
@@ -0,0 +1,21 @@
+/**
+ *
+ *  name: String - required: true
+ *  description: String
+ *  available: Boolean, default: true
+ *  price: Number - default : 0
+ *  status: Boolean - default : true - required: true
+ *  user : Schema.Types.ObjectId - required: true
+ *  category: Schema.Types.ObjectId - required: true
+ *
+ */
+
+// Obtener productos paginados - público
+
+// Obtener producto por id
+
+// Crear producto - privado - cualquier persona con un token válido
+
+// Actualizar producto - privado - cualquier persona con un token válido
+
+// Eliminar producto por di - privado - Admin
