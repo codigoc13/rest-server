5e0f17e8825ae76e6686f39543826729311ec55c
seraleman <seraleman1204@gmail.com>
2022-10-29 17:51:19 -0500

    Búsquedas en base de datos

--- controllers/search.controller.js
+++ controllers/search.controller.js
@@ -1,13 +1,43 @@
 const { request, response } = require('express')
+const { User } = require('../models')
+const { ObjectId } = require('mongoose').Types
+
+const allowedCollections = ['users', 'categories', 'products', 'roles']
+
+const searchUsers = async (searchTerm = '', res = response) => {
+  const isMongoId = ObjectId.isValid(searchTerm)
+  if (isMongoId) {
+    const user = await User.findById(searchTerm)
+    res.status(200).json({
+      results: user ? [user] : [],
+    })
+  }
+}
 
 const search = (req = request, res = response) => {
   try {
     const { collection, searchTerm } = req.params
-    res.json({
-      msg: 'buscando',
-      collection,
-      searchTerm,
-    })
+
+    if (!allowedCollections.includes(collection)) {
+      return res.status(400).json({
+        msg: 'Colección de búsqueda no existe',
+        allowedCollections,
+      })
+    }
+
+    switch (collection) {
+      case 'users':
+        searchUsers(searchTerm, res)
+        break
+      case 'categories':
+        break
+      case 'products':
+        break
+      default:
+        res.status(500).json({
+          msg: 'Búsqueda por hacer',
+        })
+    }
   } catch (error) {
     console.log(error)
     res.status(500).json({
